68950d4683c1c5eb3aff51f5ec5db1c3635c71dd
David Zhang <david.zhang@shopify.com>
2017-01-28 20:13:25 -0500

    Removed file upload code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 import python.MachineLearning as ML
 import numpy as NP
+from skimage import io
 from flask import Flask, render_template, request, redirect, url_for
 from IPython import embed
-from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = "/var/www/uploads"
 ALLOWED_EXTENSIONS = set(["jpg", "png", "jpeg", "gif"])
@@ -15,23 +15,10 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 def hello():
     return render_template("index.html")
 
-@app.route("/upload", methods=["POST"])
-def upload_picture():
-    if 'picture' not in request.files:
-        return redirect(request.url)
+@app.route("/submit", methods=["POST"])
+def submit():
+    url = request.form['url']
 
-    file = request.files['picture']
-    if file.filename == '':
-        return redirect(request.url)
-
-    if file and allowed_file(file.filename):
-        decision = ML.rate(NP.array(file))
-        return redirect(url_for('display_result', decision=decision))
-
-@app.route("/result", methods=["GET"])
-def display_result():
-    embed()
-    return render_template("result.html", decision=decision)
 
 def allowed_file(filename):
     return '.' in filename and \
